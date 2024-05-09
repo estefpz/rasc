ad90f4a34c3160b9196b8ac4bce06c1bee8051dd
Estefani P <141877727+estefpz@users.noreply.github.com>
2024-05-09 18:47:57 -0300

    soma.java

---
+++ soma.java
@@ -0,0 +1,10 @@
+public class soma {
+    public static void main(String[] args) {
+        int resultado = somar(777, 111);
+        System.out.println("A soma é: " + resultado);
+    }
+
+    public static int somar(int n1, int n2) {
+        return n1 + n2;
+    }
+}
